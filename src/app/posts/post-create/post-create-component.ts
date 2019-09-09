9440db8fdb740b780fbcad3bba0051cabc7b2360
venkatshan707 <venkatshan707@gmail.com>
2019-09-09 09:45:42 +0530

    Added Form validation( Form Handling) with Angular

--- src/app/posts/post-create/post-create-component.ts
+++ src/app/posts/post-create/post-create-component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output } from '@angular/core';
 import{Post} from '../post-model';
+import{NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -16,14 +17,17 @@ export class PostCreateComponent{
 postCreated=new EventEmitter<Post>();// It's object to emit the value with the help of output decorator
 // Adding Generic type Post for EventEmitter.
  // onAddPost(postText: HTMLTextAreaElement){
-  onAddPost(){
-
+  onAddPost( form : NgForm){
+     if(form.invalid)
+     {
+       return;
+     }
 this.newPostAcknowledgement='You posted a new Post as:';
 this.postText=this.enteredContent;
 
 const post : Post={
-  title:this.enteredTitle,
-  content:this.enteredContent,
+  title:form.value.title,
+  content:form.value.content,
 }
 this.postCreated.emit(post);// emiting the post it's listed by post-list component with
                             //the help of app.component.html
